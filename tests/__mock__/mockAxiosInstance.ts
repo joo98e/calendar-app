10b2f92c2cf7e95bcc6395c59a852ddcf9cffb30
joo98e <jtbeok@gmail.com>
2023-01-29 18:18:35 +0900

    feat - axios response mock

--- tests/__mock__/mockAxiosInstance.ts
+++ tests/__mock__/mockAxiosInstance.ts
@@ -3,20 +3,15 @@ import { CoreResponse } from "../../api/common/CustomResponse";
 const axios = require("axios");
 jest.mock("axios");
 
-export default function mockAxiosInstance() {
+type GetResponse<T> = CoreResponse<T>;
+type PostResponse<T> = CoreResponse<T>;
+type PutResponse<T> = CoreResponse<T>;
+
+
+export default function mockAxiosInstance<T, S, R>(getResponse?: GetResponse<T>, postResponse?: PostResponse<S>, putResponse?: PutResponse<R>) {
   axios.create = jest.fn().mockReturnThis();
-  axios.get.mockResolvedValue((): CoreResponse<any> => {
-    return {
-      data: null,
-      success: true
-    };
-  });
 
-  axios.post.mockResolvedValue((data): CoreResponse<any> => {
-    return {
-      data: null,
-      success: true
-    };
-  });
-
+  axios.get.mockResolvedValue(getResponse);
+  axios.post.mockResolvedValue(postResponse);
+  axios.put.mockResolvedValue(putResponse);
 }
--- tests/units/components/atoms/Typography.test.ts
+++ tests/units/components/atoms/Typography.test.ts
@@ -1,17 +1,44 @@
 import { describe } from "@jest/globals";
 import UserService from "../../../../api/user/UserService";
 import mockAxiosInstance from "../../../__mock__/mockAxiosInstance";
+import { User } from "../../../../api/user/types";
 
 const axios = require("axios");
 jest.mock("axios");
 
 describe("일반 텍스트 컴포넌트 테스트", () => {
   beforeAll(() => {
-    mockAxiosInstance();
+    const mockUser = {
+      id: 1,
+      name: "beok",
+      username: "everybeok",
+      email: "jtbeok@gmail.com",
+      address: {
+        street: "street",
+        suite: "string",
+        city: "string",
+        zipcode: "string",
+        geo: {
+          lat: "string",
+          lng: "string"
+        }
+      },
+      phone: "string",
+      website: "string",
+      company: {
+        name: "string",
+        catchPhrase: "string",
+        bs: "string"
+      }
+    };
+
+    mockAxiosInstance<User, {}, undefined>({ data: { data: mockUser }, success: true }, undefined, undefined);
   });
 
-  it("should ", async function() {
-    const users = await UserService.findAll();
+  it("should return mock value", async function() {
+    const res = await UserService.findAll();
+
+    console.log(res.data.data);
     expect(axios.get).toHaveBeenCalledTimes(1);
   });
 });
